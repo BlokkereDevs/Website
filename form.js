4303d2725b5eb5835ca0c70612239a39268e124b
BlokkereDevs <109418837+BlokkereDevs@users.noreply.github.com>
2022-09-27 01:15:12 +0530

    Subscribe form script

---
+++ form.js
@@ -0,0 +1,35 @@
+var form = document.getElementById("my-form");
+function showToast() {
+    var x = document.getElementById("snackbar");
+    x.className = "show";
+    setTimeout(function(){ x.className = x.className.replace("show", ""); }, 3000);
+}
+
+async function handleSubmit(event) {
+    event.preventDefault();
+    var status = document.getElementById("my-form-status");
+    var data = new FormData(event.target);
+    fetch(event.target.action, {
+        method: form.method,
+        body: data,
+        headers: {
+            'Accept': 'application/json'
+        }
+    }).then(response => {
+        if (response.ok) {
+        showToast();
+        form.reset()
+        } else {
+        response.json().then(data => {
+            if (Object.hasOwn(data, 'errors')) {
+            status.innerHTML = data["errors"].map(error => error["message"]).join(", ")
+            } else {
+            status.innerHTML = "Oops! There was a problem submitting your form"
+            }
+        })
+        }
+    }).catch(error => {
+        status.innerHTML = "Oops! There was a problem submitting your form"
+    });
+}
+form.addEventListener("submit", handleSubmit)
